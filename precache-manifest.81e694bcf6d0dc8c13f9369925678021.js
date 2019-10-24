0f49e98172d6c279e51a095e1d77e7b0b5a1fea3
Sven Hellsten <svenhellsten@Svens-MacBook-Pro.local>
2019-10-24 18:55:39 +0200

    Updates

---
+++ precache-manifest.81e694bcf6d0dc8c13f9369925678021.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126c67ce0dcc854e685e1d74dcd42cde",
+    "url": "/apk/index.html"
+  },
+  {
+    "revision": "51bd63c260c7766efbbf",
+    "url": "/apk/static/css/main.5bd73751.chunk.css"
+  },
+  {
+    "revision": "ca84e3ca985b81e318c1",
+    "url": "/apk/static/js/2.75e951ca.chunk.js"
+  },
+  {
+    "revision": "51bd63c260c7766efbbf",
+    "url": "/apk/static/js/main.9c78c48e.chunk.js"
+  },
+  {
+    "revision": "15d39b9da93a0c014110",
+    "url": "/apk/static/js/runtime-main.af2eedc5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/apk/static/media/logo.25bf045c.svg"
+  }
+]);
